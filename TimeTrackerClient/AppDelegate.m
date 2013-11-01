ed80e3eb0a8996a5f186c8e0d77993958a478900
Marcin Swierczynski <marcin@uws.ie>
2013-11-01 12:13:30 +0100

    App skeleton + dependencies

---
+++ TimeTrackerClient/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  TimeTrackerClient
+//
+//  Created by Marcin Swierczynski on 01.11.2013.
+//  Copyright (c) 2013 UWS. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+
+}
+
+@end
---
+++ TimeTrackerClient/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TimeTrackerClient
+//
+//  Created by Marcin Swierczynski on 01.11.2013.
+//  Copyright (c) 2013 UWS. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
